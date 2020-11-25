c881caa7e15e1f27dd52d4167f11e3f462db6078
m <2662757915@qq.com>
2020-11-25 19:43:06 +0800

    init

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -63,98 +63,143 @@ export default {
                 },
                 {
                     icon: 'el-icon-lx-cascades',
-                    index: 'table',
-                    title: '基础表格'
-                },
-                {
-                    icon: 'el-icon-lx-copy',
-                    index: 'tabs',
-                    title: 'tab选项卡'
-                },
-                {
-                    icon: 'el-icon-lx-calendar',
-                    index: '3',
-                    title: '表单相关',
+                    index: 'customerManage',
+                    title: '客户管理',
                     subs: [
                         {
-                            index: 'form',
-                            title: '基本表单'
-                        },
-                        {
-                            index: '3-2',
-                            title: '三级菜单',
-                            subs: [
-                                {
-                                    index: 'editor',
-                                    title: '富文本编辑器'
-                                },
-                                {
-                                    index: 'markdown',
-                                    title: 'markdown编辑器'
-                                }
-                            ]
-                        },
-                        {
-                            index: 'upload',
-                            title: '文件上传'
+                            index: 'customer/index',
+                            title: '客户列表'
                         }
                     ]
                 },
                 {
-                    icon: 'el-icon-lx-emoji',
-                    index: 'icon',
-                    title: '自定义图标'
-                },
-                {
-                    icon: 'el-icon-pie-chart',
-                    index: 'charts',
-                    title: 'schart图表'
-                },
-                {
-                    icon: 'el-icon-rank',
-                    index: '6',
-                    title: '拖拽组件',
+                    icon: 'el-icon-lx-cascades',
+                    index: 'goodManage',
+                    title: '商品管理',
                     subs: [
                         {
-                            index: 'drag',
-                            title: '拖拽列表'
-                        },
-                        {
-                            index: 'dialog',
-                            title: '拖拽弹框'
+                            index: 'good/index',
+                            title: '商品列表'
                         }
                     ]
                 },
                 {
-                    icon: 'el-icon-lx-global',
-                    index: 'i18n',
-                    title: '国际化功能'
-                },
-                {
-                    icon: 'el-icon-lx-warn',
-                    index: '7',
-                    title: '错误处理',
+                    icon: 'el-icon-lx-cascades',
+                    index: 'orderManage',
+                    title: '订单管理',
                     subs: [
                         {
-                            index: 'permission',
-                            title: '权限测试'
-                        },
-                        {
-                            index: '404',
-                            title: '404页面'
+                            index: 'order/index',
+                            title: '订单列表'
                         }
                     ]
                 },
                 {
-                    icon: 'el-icon-lx-redpacket_fill',
-                    index: '/donate',
-                    title: '支持作者'
+                    icon: 'el-icon-lx-cascades',
+                    index: 'stockManage',
+                    title: '进货单管理',
+                    subs: [
+                        {
+                            index: 'stock/index',
+                            title: '进货单列表'
+                        }
+                    ]
                 }
+                // {
+                //     icon: 'el-icon-lx-cascades',
+                //     index: 'table',
+                //     title: '基础表格'
+                // },
+                // {
+                //     icon: 'el-icon-lx-copy',
+                //     index: 'tabs',
+                //     title: 'tab选项卡'
+                // },
+                // {
+                //     icon: 'el-icon-lx-calendar',
+                //     index: '3',
+                //     title: '表单相关',
+                //     subs: [
+                //         {
+                //             index: 'form',
+                //             title: '基本表单'
+                //         },
+                //         {
+                //             index: '3-2',
+                //             title: '三级菜单',
+                //             subs: [
+                //                 {
+                //                     index: 'editor',
+                //                     title: '富文本编辑器'
+                //                 },
+                //                 {
+                //                     index: 'markdown',
+                //                     title: 'markdown编辑器'
+                //                 }
+                //             ]
+                //         },
+                //         {
+                //             index: 'upload',
+                //             title: '文件上传'
+                //         }
+                //     ]
+                // },
+                // {
+                //     icon: 'el-icon-lx-emoji',
+                //     index: 'icon',
+                //     title: '自定义图标'
+                // },
+                // {
+                //     icon: 'el-icon-pie-chart',
+                //     index: 'charts',
+                //     title: 'schart图表'
+                // },
+                // {
+                //     icon: 'el-icon-rank',
+                //     index: '6',
+                //     title: '拖拽组件',
+                //     subs: [
+                //         {
+                //             index: 'drag',
+                //             title: '拖拽列表'
+                //         },
+                //         {
+                //             index: 'dialog',
+                //             title: '拖拽弹框'
+                //         }
+                //     ]
+                // },
+                // {
+                //     icon: 'el-icon-lx-global',
+                //     index: 'i18n',
+                //     title: '国际化功能'
+                // },
+                // {
+                //     icon: 'el-icon-lx-warn',
+                //     index: '7',
+                //     title: '错误处理',
+                //     subs: [
+                //         {
+                //             index: 'permission',
+                //             title: '权限测试'
+                //         },
+                //         {
+                //             index: '404',
+                //             title: '404页面'
+                //         }
+                //     ]
+                // },
+                // {
+                //     icon: 'el-icon-lx-redpacket_fill',
+                //     index: '/donate',
+                //     title: '支持作者'
+                // }
             ]
         };
     },
     computed: {
         onRoutes() {
+            console.log(this.$route.path.replace('/', ''));
             return this.$route.path.replace('/', '');
         }
     },
--- src/components/page/good/index.vue
+++ src/components/page/good/index.vue
@@ -1,12 +1,5 @@
 <template>
     <div>
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item>
-                    <i class="el-icon-lx-cascades"></i> 基础表格
-                </el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
         <div class="container">
             <div class="handle-box">
                 <el-button
@@ -102,7 +95,7 @@
 </template>
 
 <script>
-import { fetchData } from '../../api/index';
+import { fetchData } from '../../../api/index';
 export default {
     name: 'basetable',
     data() {
@@ -129,11 +122,50 @@ export default {
     methods: {
         // 获取 easy-mock 的模拟数据
         getData() {
-            fetchData('good').then(res => {
-                console.log(res);
-                this.tableData = res.list;
-                this.pageTotal = res.pageTotal || 50;
-            });
+            // fetchData('good').then(res => {
+            //     console.log(res);
+            //     this.tableData = res.list;
+            //     this.pageTotal = res.pageTotal || 50;
+            // });
+            this.tableData = [
+                {
+                    id: 1,
+                    name: '张三',
+                    money: 123,
+                    address: '广东省东莞市长安镇',
+                    state: '成功',
+                    date: '2019-11-1',
+                    thumb: 'https://lin-xin.gitee.io/images/post/wms.png'
+                },
+                {
+                    id: 2,
+                    name: '李四',
+                    money: 456,
+                    address: '广东省广州市白云区',
+                    state: '成功',
+                    date: '2019-10-11',
+                    thumb: 'https://lin-xin.gitee.io/images/post/node3.png'
+                },
+                {
+                    id: 3,
+                    name: '王五',
+                    money: 789,
+                    address: '湖南省长沙市',
+                    state: '失败',
+                    date: '2019-11-11',
+                    thumb: 'https://lin-xin.gitee.io/images/post/parcel.png'
+                },
+                {
+                    id: 4,
+                    name: '赵六',
+                    money: 1011,
+                    address: '福建省厦门市鼓浪屿',
+                    state: '成功',
+                    date: '2019-10-20',
+                    thumb: 'https://lin-xin.gitee.io/images/post/notice.png'
+                }
+            ];
+            this.pageTotal = 4
         },
         // 触发搜索按钮
         handleSearch() {
